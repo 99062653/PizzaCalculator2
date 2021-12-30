a7f164ee1345ebfeeed7fde6fc40f1f7e49024e2
Rick Huisman <rick@idea-x.nl>
2021-12-30 19:17:55 +0100

    1.5 done

--- PizzaCalculator.cs
+++ PizzaCalculator.cs
@@ -2,18 +2,19 @@
 {
     class Program
     {
-        public static double smallPizaPrice, mediumPizaPrice, largePizaPrice;
-        public static double Cart;
+        public static double smallPizzaPrice, mediumPizzaPrice, largePizzaPrice;
         public static int cartAmount;
+        public static double cartPrice;
         public static string pizzaChoiceMade = "";
+        public static double pizzaChoicePrice;
         static void Settings()
         {
-            smallPizaPrice = 6.25;
-            mediumPizaPrice = 12.25;
-            largePizaPrice = 19.95;
+            smallPizzaPrice = 6.25;
+            mediumPizzaPrice = 12.25;
+            largePizzaPrice = 19.95;
 
-            Cart = 0;
             cartAmount = 0;
+            cartPrice = 0.0;
 
             Console.OutputEncoding = System.Text.Encoding.UTF8;
             Console.BackgroundColor = ConsoleColor.DarkRed;
@@ -25,9 +26,9 @@
         {
             Console.WriteLine("---------RICK'S PIZZA---------");
             Console.WriteLine("WELKE MAAT WILT U BESTELLEN");
-            Console.WriteLine($"(S)MALL: \u20AC{smallPizaPrice}");
-            Console.WriteLine($"(M)EDIUM: \u20AC{mediumPizaPrice}");
-            Console.WriteLine($"(L)ARGE: \u20AC{largePizaPrice}");
+            Console.WriteLine($"(S)MALL: \u20AC{smallPizzaPrice}");
+            Console.WriteLine($"(M)EDIUM: \u20AC{mediumPizzaPrice}");
+            Console.WriteLine($"(L)ARGE: \u20AC{largePizzaPrice}");
             Console.WriteLine("------------------------------");
             pizzaChoiceMade = Console.ReadKey().Key.ToString();
 
@@ -44,14 +45,17 @@
                 
                 case "S": case "SMALL":
                     pizzaChoiceMade = "SMALL";
+                    pizzaChoicePrice = smallPizzaPrice;
                     break;
                 
                 case "M": case "MEDIUM":
                     pizzaChoiceMade = "MEDIUM";
+                    pizzaChoicePrice = mediumPizzaPrice;
                     break;
                 
                 case "L": case "LARGE":
                     pizzaChoiceMade = "LARGE";
+                    pizzaChoicePrice = largePizzaPrice;
                     break;
             }
             Console.WriteLine($"HOEVEEL {pizzaChoiceMade} PIZZA'S WILT U? ");
@@ -76,7 +80,25 @@
         }
         static void checkoutPizza()
         {
+            Console.WriteLine("---------RICK'S PIZZA---------");
+            Console.WriteLine("HIER IS UW BON!");
+            
+            for (int r = 0; r < cartAmount; r++)
+            {
+                cartPrice += pizzaChoicePrice;
+                Console.WriteLine($"1 {pizzaChoiceMade} PIZZA = \u20AC{pizzaChoicePrice}");
+            }
+            cartPrice = Math.Round(cartPrice, 2);
+            Console.WriteLine($"AANTAL = \u20AC{cartPrice}");
+            Console.WriteLine("------------------------------");
+            Console.WriteLine("DRUK OP ENTER OM TE BETALEN");
 
+            if (Console.ReadKey().Key == ConsoleKey.Enter)
+            {
+                Console.WriteLine($"OVER 15 MINUTEN STAAN WE VOOR JE DEUR {Environment.MachineName} ;)");
+                Console.ReadKey();
+                Environment.Exit(0);
+            }
         }
         static void Main()
         {   
